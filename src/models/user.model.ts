ebdf45b425e99d5154badc7c6cd3a55007b088ba
AnilDevchhetri <chhetrivision@gmail.com>
2026-03-19 22:13:42 +0900

    57

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -35,3 +35,7 @@ const userSchema = new mongoose.Schema<IUser>(
   },
   { timestamps: true },
 );
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
