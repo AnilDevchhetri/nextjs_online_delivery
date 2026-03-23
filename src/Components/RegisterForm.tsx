f6a1ead3cda94e6d60ebc60a36dfa9c1e8e69aa3
AnilDevchhetri <chhetrivision@gmail.com>
2026-03-23 21:47:12 +0900

    4:20

--- src/Components/RegisterForm.tsx
+++ src/Components/RegisterForm.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 
 import googleImage from '@/assets/google.png'
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 type PropType = {
     previousStep: (s: number) => void;
@@ -16,6 +17,7 @@ function RegisterForm({ previousStep }: PropType) {
     const [password, setPassword] = useState("");
     const [passwordType, setPasswordType] = useState("password");
     const [loading, setLoading] = useState(false);
+    const router = useRouter();
     const handleRegister = async (e: React.FormEvent) => {
         e.preventDefault();
         if (loading) return;
@@ -109,7 +111,7 @@ function RegisterForm({ previousStep }: PropType) {
                     Sign With Google
                 </button>
             </motion.form>
-            <p className='text-gray-600 mt-6 text-sm flex items-center gap-1 cursor-pointer'>Already have an account ? <LogIn className='w-4 h-4' /><span className='text-green-600'> Sign in</span></p>
+            <p className='text-gray-600 mt-6 text-sm flex items-center gap-1 cursor-pointer' onClick={() => router.push('/login')}>Already have an account ? <LogIn className='w-4 h-4' /><span className='text-green-600'> Sign in</span></p>
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Provider from "@/Provider";
 
 
 export const metadata: Metadata = {
@@ -15,7 +16,11 @@ export default function RootLayout({
   return (
     <html
       lang="en">
-      <body className="w-full min-h-screen bg-linear-to-b from-green-100 to-white">{children}</body>
+      <body className="w-full min-h-screen bg-linear-to-b from-green-100 to-white">
+        <Provider>
+          {children}
+        </Provider>
+      </body>
     </html>
   );
 }
